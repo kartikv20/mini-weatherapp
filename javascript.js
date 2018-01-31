862d564fb953a06a410071aba0c6a89d174afc65
kartikv20 <kartikv20@gmail.com>
2018-01-31 13:36:57 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
-
-
 // Invoke function on load
-(function(){
+function getLocation(){
 	if(navigator.geolocation){
 		navigator.geolocation.getCurrentPosition(function(position){
 			var lng = position.coords.longitude;
@@ -13,23 +11,24 @@
 		console.log("Browser not supported");
 	}
 	
-})();
+};
 
 
 
 function getWeather(lat, lng){
-	let api = 'https://api.openweathermap.org/data/2.5/weather?'; // we defined here since it's related to this function - 
+	let api = 'https://fcc-weather-api.glitch.me/api/current?'; // we defined here since it's related to this function - 
 	// we don't want to pollute the global window.
 	
-	fetch(`${api}/lat=${lat}&lon=${lng}`) // we use javascript template literals
+	fetch(`${api}lat=${lat}&lon=${lng}`) // we use javascript template literals
 	.then(res => res.json())
 	.then(res => {
-		console.log(res)
+		document.getElementById("loc").innerText = res.name;
+		document.getElementById("tempp").innerText = res.main.temp;
 	}).catch(err => {
 		console.log('An error occured: ', err) //catch errors
 	})
 }
 
-
-
-
+window.onload = function() {
+  document.getElementById("getloc").addEventListener('click',getLocation); 
+};
