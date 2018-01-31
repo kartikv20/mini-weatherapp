d25c95c8244b8aed3fa6cd43c03914b5174a7ffa
kartikv20 <kartikv20@gmail.com>
2018-01-31 10:57:35 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,33 @@
-navigator.geolocation.getCurrentPosition(function(position){
-	console.log(position);
-});
+var api = 'api.openweathermap.org/data/2.5/weather?';
+
+function getLocation(){
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(function(position){
+			var lng = position.coords.longitude;
+			var latt = position.coords.latitude;
+			getWeather(latt,lng);
+		});
+	}
+	else{
+		console.log("Browser not supported");
+	}
+}
+
+
+function getWeather(latt, lng)
+{
+	var myUrl = api+"lat="+latt+"&+lon="+lng;
+	fetch(myUrl)
+	.then(response=>response.json())
+	.then(data=>console.log(data.main);
+
+	)
+
+}
+
+
+
+
+window.onload = function() {
+  document.getElementById("getloc").addEventListener('click',getLocation); 
+};
